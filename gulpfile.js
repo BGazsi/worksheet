31e8519c3abf6af3cfc7629e1b5c7f58c737513b
Dora <honti.dora@virgo.hu>
2018-05-17 13:29:25 +0200

    gulp js

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,25 @@
 var gulp = require('gulp'),
     rename = require('gulp-rename'),
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    babel = require('gulp-babel');
 
-gulp.task('sass', function () {
+gulp.task('scss', function () {
     return gulp.src('./public/src/scss/**/init.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(rename('style.css'))
     .pipe(gulp.dest('./public/dist/css/'));
 });
 
-gulp.task('watch', function () {
-    gulp.watch('./public/src/scss/**/*.scss', ['sass']);
+gulp.task('js', function () {
+    return gulp.src('./public/src/js/**/*.js')
+    .pipe(babel())
+    .pipe(rename('script.js'))
+    .pipe(gulp.dest('./public/dist/js/'));
 });
 
-gulp.task('default', ['sass']);
+gulp.task('watch', function () {
+    gulp.watch('./public/src/scss/**/*.scss', ['scss']);
+    gulp.watch('./public/src/js/**/*.js', ['js']);
+});
+
+gulp.task('default', ['scss', 'js']);
